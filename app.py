a9b21008b5e327758912e120fea8426784840430
abok-cymk <allanabok18@gmail.com>
2024-10-23 17:38:49 +0300

    finished the registration definition logic

--- app.py
+++ app.py
@@ -21,4 +21,30 @@ def register():
         if not username or not username.isalpha():
             flash("username must only contain letters!")
             return redirect(url_for('register'))
+        
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(email_pattern, email):
+            flash("Inavlid email format")
+            return redirect(url_for('register'))
+        
+        password_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$'
+        if len(password) < 8 or not re.match(password_pattern, password):
+            flash("Password must contain at least one uppercase letter, one lowercase letter, one number and be at least 8 characters!")
+            return redirect(url_for('register'))
+        
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        users[username] = {
+            'email': email,
+            'password': password,
+        }
+        
+        flash('Registration successful!')
+        return redirect(url_for('home'))
+    
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+            
     
